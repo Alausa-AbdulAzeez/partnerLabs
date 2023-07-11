2d5f5345ab9f04898d163e130d42fe1ebd953781
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-07-11 18:55:07 +0100

    modified homeDatagrid and changePassword routes

--- src/components/homeDatagrid/Homedatagrid.jsx
+++ src/components/homeDatagrid/Homedatagrid.jsx
@@ -1,36 +1,43 @@
-import { Box } from "@mui/system";
-import { DataGrid } from "@mui/x-data-grid";
-import React, { useEffect, useState } from "react";
-import "./homedatagrid.scss";
-import { useSelector } from "react-redux";
-import { format } from "date-fns";
+import { Box } from '@mui/system'
+import { DataGrid } from '@mui/x-data-grid'
+import React, { useEffect, useState } from 'react'
+import './homedatagrid.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { format } from 'date-fns'
+import { loggedOut } from '../../redux/globalSlice'
+import { persistor } from '../../redux/store'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Homedatagrid = (props) => {
-  const [pageSize, setPageSize] = useState(5);
+  // MISCELLANEOUS
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [pageSize, setPageSize] = useState(5)
 
   // TABLE DATA
-  const tableData = props?.tableData;
-  console.log(tableData);
-  let rows;
-  let columns;
-  let title;
+  const tableData = props?.tableData
+  console.log(tableData)
+  let rows
+  let columns
+  let title
 
   // END OF TABLE DATA
 
-  const { currentUser } = useSelector((state) => state?.user);
-  const loggedInUserRole = currentUser?.data?.role;
+  const { currentUser } = useSelector((state) => state?.user)
+  const loggedInUserRole = currentUser?.data?.role
 
   const defaultColumns = [
     {
-      field: "candidateName",
-      headerName: "Candidate Name",
+      field: 'candidateName',
+      headerName: 'Candidate Name',
       width: 250,
       editable: true,
     },
-    { field: "clientName", headerName: "Company Name", width: 190 },
+    { field: 'clientName', headerName: 'Company Name', width: 190 },
     {
-      field: "testcategory",
-      headerName: "Test Category",
+      field: 'testcategory',
+      headerName: 'Test Category',
       width: 140,
       editable: false,
     },
@@ -46,8 +53,8 @@ const Homedatagrid = (props) => {
     //   },
     // },
     {
-      field: "phoneNumber",
-      headerName: "Phone Number",
+      field: 'phoneNumber',
+      headerName: 'Phone Number',
       width: 130,
     },
     // {
@@ -66,47 +73,70 @@ const Homedatagrid = (props) => {
     //     );
     //   },
     // },
-  ];
+  ]
 
   switch (loggedInUserRole) {
-    case "Reception":
-      rows = tableData;
-      columns = defaultColumns;
-      title = "Candidates";
-      break;
-    case "Phlebotomy":
-      rows = tableData;
-      columns = defaultColumns;
-      title = "Candidates";
-      break;
-    case "MainLab1":
-      rows = tableData;
-      columns = defaultColumns;
-      title = "Candidates";
-      break;
-    case "Quality assurance":
-      rows = tableData;
-      columns = defaultColumns;
-      title = "Candidates";
-      break;
-    case "Report":
-      rows = tableData;
-      columns = defaultColumns;
-      title = "Candidates";
-      break;
+    case 'Reception':
+      rows = tableData
+      columns = defaultColumns
+      title = 'Candidates'
+      break
+    case 'Phlebotomy':
+      rows = tableData
+      columns = defaultColumns
+      title = 'Candidates'
+      break
+    case 'MainLab1':
+      rows = tableData
+      columns = defaultColumns
+      title = 'Candidates'
+      break
+    case 'Quality assurance':
+      rows = tableData
+      columns = defaultColumns
+      title = 'Candidates'
+      break
+    case 'Report':
+      rows = tableData
+      columns = defaultColumns
+      title = 'Candidates'
+      break
 
     default:
-      break;
+      break
   }
 
-  useEffect(() => {}, [tableData]);
+  useEffect(() => {}, [tableData])
+
+  useEffect(() => {
+    if (loggedInUserRole === 'Client' || loggedInUserRole === 'SuperAdmin') {
+      dispatch(loggedOut())
+
+      persistor
+        .purge()
+        // .then(() => navigate('/login'))
+        .then(() => {
+          return toast.info('Session Expired Please login to continue', {
+            position: 'top-right',
+            autoClose: 2500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+          })
+        })
+    }
+  }, [])
+
   return (
-    <div className="datagridWraper">
+    <div className='datagridWraper'>
       <h3>{title}</h3>
-      <Box sx={{ height: 350, width: "100%" }}>
+      <Box sx={{ height: 350, width: '100%' }}>
         <DataGrid
-          rows={rows}
-          columns={columns}
+          rows={tableData}
+          columns={defaultColumns}
           pageSize={pageSize}
           // checkboxSelection
           // disableSelectionOnClick
@@ -118,7 +148,7 @@ const Homedatagrid = (props) => {
         />
       </Box>
     </div>
-  );
-};
+  )
+}
 
-export default Homedatagrid;
+export default Homedatagrid
--- src/pages/changePassword/ChangePassword.jsx
+++ src/pages/changePassword/ChangePassword.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import "./changePassword.scss";
-import { BsEye, BsFillEyeSlashFill } from "react-icons/bs";
-import { publicRequest } from "../../functions/requestMethods";
+import React, { useState, useEffect, useRef } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import './changePassword.scss'
+import { BsEye, BsFillEyeSlashFill } from 'react-icons/bs'
+import { publicRequest } from '../../functions/requestMethods'
 // import { login } from '../../redux/apiCalls'
-import { useDispatch, useSelector } from "react-redux";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { login } from "../../redux/apiCalls";
+import { useDispatch, useSelector } from 'react-redux'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { login } from '../../redux/apiCalls'
 
 const ChangePassword = () => {
   // MISCELLANEOUS
-  const [btnDisabled, setBtnDisabled] = useState(false);
-  const token = useSelector((state) => state?.user?.currentUser?.data?.token);
+  const [btnDisabled, setBtnDisabled] = useState(false)
+  const token = useSelector((state) => state?.user?.currentUser?.data?.token)
 
-  const toastId = useRef(null);
+  const toastId = useRef(null)
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   // END OF MISCELLANEOUS
 
   // USER LOGIN DETAILS
   const [user, setUser] = useState({
-    username: "",
-    oldPassword: "",
-    newPassword: "",
-    confirmPassword: "",
-  });
+    username: '',
+    oldPassword: '',
+    newPassword: '',
+    confirmPassword: '',
+  })
   // END OF USER LOGIN DETAILS
 
   //   FUNCTIONs FOR SETTING BUTTON STATE
 
   const handleSetUser = (event, inputType) => {
-    setUser({ ...user, [inputType]: event.target.value });
-  };
+    setUser({ ...user, [inputType]: event.target.value })
+  }
 
   //   END OF FUNCTIONs FOR SETTING BUTTON STATE
 
   // PASSWORD TOGGLE FUNCTIONALITY
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false)
 
   // FUNCTION FOR PASSWORD TOGGLE
   const handlePasswordToggle = () => {
-    setShowPassword((prev) => !prev);
-  };
+    setShowPassword((prev) => !prev)
+  }
 
   // END OF FUNCTION FOR PASSWORD TOGGLE
 
@@ -53,22 +53,22 @@ const ChangePassword = () => {
   // FUNCTION FOR ONCLICK LOGIN BUTTON
   const handleChangePassword = async (e) => {
     // PREVENT PAGE RELOAD
-    e.preventDefault();
+    e.preventDefault()
 
     // DISABLE "CONFIRM" BUTTON
-    setBtnDisabled(true);
+    setBtnDisabled(true)
 
-    toastId.current = toast("Please Wait", {
+    toastId.current = toast('Please Wait', {
       autoClose: 3000,
       isLoading: true,
-    });
+    })
 
     try {
       if (user?.newPassword.trim() === user?.confirmPassword.trim()) {
         const res = await publicRequest
-          .post("/Account/change-password", user, {
+          .post('/Account/change-password', user, {
             headers: {
-              "Content-Type": "application/json",
+              'Content-Type': 'application/json',
               Authorization: `Bearer ${token}`,
             },
           })
@@ -76,123 +76,129 @@ const ChangePassword = () => {
             toast.update(toastId.current, {
               render:
                 "Password updated succesfully! You'll be redirected in a bit, Please wait...",
-              type: "success",
+              type: 'success',
               isLoading: false,
               autoClose: 3000,
-            });
+            })
 
-            setBtnDisabled(false);
+            setBtnDisabled(false)
 
             setTimeout(() => {
-              navigate("/login");
-            }, 2500);
-          });
-        console.log(res);
+              navigate('/login')
+            }, 2500)
+          })
+        console.log(res)
       } else {
         toast.update(toastId.current, {
           render: "Passwords don't match!",
-          type: "error",
+          type: 'error',
           isLoading: false,
           autoClose: 2000,
-        });
+        })
 
         setTimeout(() => {
-          setBtnDisabled(false);
-        }, 2000);
+          setBtnDisabled(false)
+        }, 2000)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
       toast.update(toastId.current, {
-        type: "error",
+        type: 'error',
         autoClose: 2500,
         isLoading: false,
         render: `${
           error?.response?.data?.title ||
           error?.response?.data?.description ||
           error?.message ||
-          "Something went wrong, please try again"
+          'Something went wrong, please try again'
         }`,
-      });
-      setBtnDisabled(false);
+      })
+      setBtnDisabled(false)
     }
 
     // login(dispatch, user, navigate)
-  };
+  }
   // END OF FUNCTION FOR ONCLICK LOGIN BUTTON
 
   //   USE EFFECT FOR SETTING BUTTON STATE
   useEffect(() => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  }, [user])
+
+  useEffect(() => {
+    toast.info(
+      "Hi there! You're required to change your password from the default password before you can proceed."
+    )
+  }, [])
 
   return (
     <>
       <ToastContainer />
-      <div className="changePasswordWrapper">
-        <div className="changePasswordWrapperRight">
+      <div className='changePasswordWrapper'>
+        <div className='changePasswordWrapperRight'>
           <form
-            className="changePasswordFormWrapper"
+            className='changePasswordFormWrapper'
             onSubmit={handleChangePassword}
           >
-            <div className="changePasswordHeading">Change Password</div>
-            <div className="changePasswordInputs">
-              <label htmlFor="">Email</label>
+            <div className='changePasswordHeading'>Change Password</div>
+            <div className='changePasswordInputs'>
+              <label htmlFor=''>Email</label>
               <input
-                type="email"
-                className="changePasswordEmailInput changePasswordInput"
-                placeholder="example@****.com"
-                data-testid="emailTestId"
-                onChange={(e) => handleSetUser(e, "username")}
+                type='email'
+                className='changePasswordEmailInput changePasswordInput'
+                placeholder='example@****.com'
+                data-testid='emailTestId'
+                onChange={(e) => handleSetUser(e, 'username')}
                 required
               />
 
-              <label htmlFor="">Old Password</label>
-              <div className="passwordWrapper">
+              <label htmlFor=''>Old Password</label>
+              <div className='passwordWrapper'>
                 <input
-                  type={showPassword ? "text" : "password"}
-                  className="changePasswordPasswordInput changePasswordInput"
-                  placeholder="Password"
-                  onChange={(e) => handleSetUser(e, "oldPassword")}
-                  data-testid="passwordTestId"
+                  type={showPassword ? 'text' : 'password'}
+                  className='changePasswordPasswordInput changePasswordInput'
+                  placeholder='Password'
+                  onChange={(e) => handleSetUser(e, 'oldPassword')}
+                  data-testid='passwordTestId'
                   required
                 />
               </div>
-              <label htmlFor="">New Password</label>
-              <div className="passwordWrapper">
+              <label htmlFor=''>New Password</label>
+              <div className='passwordWrapper'>
                 <input
-                  type={showPassword ? "text" : "password"}
-                  className="changePasswordPasswordInput changePasswordInput"
-                  placeholder="Password"
-                  onChange={(e) => handleSetUser(e, "newPassword")}
-                  data-testid="passwordTestId"
+                  type={showPassword ? 'text' : 'password'}
+                  className='changePasswordPasswordInput changePasswordInput'
+                  placeholder='Password'
+                  onChange={(e) => handleSetUser(e, 'newPassword')}
+                  data-testid='passwordTestId'
                   required
                 />
               </div>
-              <label htmlFor="">Confirm New Password</label>
-              <div className="passwordWrapper">
+              <label htmlFor=''>Confirm New Password</label>
+              <div className='passwordWrapper'>
                 <input
-                  type={showPassword ? "text" : "password"}
-                  className="changePasswordPasswordInput changePasswordInput"
-                  placeholder="Password"
-                  onChange={(e) => handleSetUser(e, "confirmPassword")}
-                  data-testid="passwordTestId"
+                  type={showPassword ? 'text' : 'password'}
+                  className='changePasswordPasswordInput changePasswordInput'
+                  placeholder='Password'
+                  onChange={(e) => handleSetUser(e, 'confirmPassword')}
+                  data-testid='passwordTestId'
                   required
                 />
               </div>
-              <div className="checkboxWrapper">
-                <label htmlFor="">Show Passwords</label>
+              <div className='checkboxWrapper'>
+                <label htmlFor=''>Show Passwords</label>
                 <input
-                  type="checkbox"
-                  className="checkbox"
+                  type='checkbox'
+                  className='checkbox'
                   onChange={handlePasswordToggle}
                 />
               </div>
             </div>
 
             <button
-              className="changePasswordBtn"
-              type={"submit"}
-              data-testid="changePasswordBtn"
+              className='changePasswordBtn'
+              type={'submit'}
+              data-testid='changePasswordBtn'
               disabled={btnDisabled}
             >
               Confirm
@@ -201,7 +207,7 @@ const ChangePassword = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default ChangePassword;
+export default ChangePassword
